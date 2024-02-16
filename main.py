bec6efd670353a1b0ff7fcdea0c1808f8d8ecc2e
Alan <113253976+MrDonkey08@users.noreply.github.com>
2024-02-16 14:12:40 -0600

    my_data def

--- main.py
+++ main.py
@@ -5,15 +5,41 @@
 CHUNK_SIZE = 5
 
 original_string = "TGTAGTGCAGTGGCGTGATCTTGGCTCACTGCAGCCTCCACCTTAGAGCAATCCTCTTGCCTCATCCTCCCGGGTAGTTGGGACTACATGTGCATGCCACATGCCTGGCTAATTTTTGTATTTTTAGTA"
+pos = [15, 100, 54, 33, 19, 97, 13]
+ref = "TATGCCC" 
+alt = "AGAAGTA"
 
-class my_string:
-    def __init__(self, pos, ref, alt, string):
+class my_data:
+    def __init__(self, pos, ref, alt):
         self.pos = pos
         self.ref = ref
         self.alt = alt
-        self.string = string
 
-        # Sizes 
+    def __str__(self):
+        return f"my_data: pos={self.pos}, ref='{self.ref}', alt='{self.alt}'"
+
+    def bubble_sort(self):
+        self.ref = list(self.ref)
+        self.alt = list(self.alt)
+
+        n = len(self.pos)
+
+        for i in range(n):
+            swapped = False
+            for j in range(0, n-i-1):
+                if self.pos[j] > self.pos[j+1]:
+                    self.pos[j], self.pos[j+1] = self.pos[j+1], self.pos[j]
+                    self.ref[j], self.ref[j+1] = self.ref[j+1], self.ref[j]
+                    self.alt[j], self.alt[j+1] = self.alt[j+1], self.alt[j]
+                    swapped = True
+            if not swapped:
+                self.ref = ''.join(self.ref)
+                self.alt = ''.join(self.alt)
+                break
+
+class my_string:
+    def __init__(self, string):
+        self.string = string
         self.size = len(self.string)
         self.cos = self.size // CHUNK_SIZE
         self.res = self.size % CHUNK_SIZE
@@ -46,8 +72,11 @@ class my_string:
 
         print()
 
-
-main_string = my_string(0, "", "", original_string)
+main_string = my_string(original_string)
 
 main_string.print_chunk()
 
+data = my_data(pos, ref, alt)
+data.bubble_sort()
+
+print(data)

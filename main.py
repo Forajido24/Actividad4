4edd0bf48dc075dcb7e071ce81c136a6bd5fcd05
Alan <113253976+MrDonkey08@users.noreply.github.com>
2024-02-16 16:50:58 -0600

    Combination def

--- main.py
+++ main.py
@@ -12,16 +12,13 @@ alt = "AGAAGTA"
 class my_data:
     def __init__(self, pos, ref, alt):
         self.pos = pos
-        self.ref = ref
-        self.alt = alt
+        self.ref = list(ref)
+        self.alt = list(alt)
 
     def __str__(self):
         return f"my_data: pos={self.pos}, ref='{self.ref}', alt='{self.alt}'"
 
     def bubble_sort(self):
-        self.ref = list(self.ref)
-        self.alt = list(self.alt)
-
         n = len(self.pos)
 
         for i in range(n):
@@ -33,22 +30,21 @@ class my_data:
                     self.alt[j], self.alt[j+1] = self.alt[j+1], self.alt[j]
                     swapped = True
             if not swapped:
-                self.ref = ''.join(self.ref)
-                self.alt = ''.join(self.alt)
                 break
 
 class my_string:
-    def __init__(self, string):
-        self.string = string
+    def __init__(self):
+        self.string = list(original_string)
         self.size = len(self.string)
         self.cos = self.size // CHUNK_SIZE
         self.res = self.size % CHUNK_SIZE
+        self.num_chunks = self.cos + (1 if self.res != 0 else 0) # If res != 0, then add 1, else add 0
 
     def print_chunk(self):
         print("String\n")
 
         print("Size: ", self.size)
-        print("Numbers of Chunks: ", self.cos)
+        print("Numbers of Chunks: ", self.num_chunks)
         print("Last chunk size: ", self.res, end='\n\n')
 
         print("Chunks\n")
@@ -57,7 +53,7 @@ class my_string:
 
         for char in range(self.cos):
             print(i, "\t", end='')
-            for i in range(i, i+CHUNK_SIZE):
+            for i in range(i, i + CHUNK_SIZE):
                 print(self.string[i], end='')
                 #print(i)
             i += 1
@@ -72,11 +68,36 @@ class my_string:
 
         print()
 
-main_string = my_string(original_string)
+def gen_comb(data, string):
+    strings = []
+    j = 0
 
-main_string.print_chunk()
+    for i in range(string.num_chunks):
+        strings.append(my_string())
+        try:
+            n = data.pos.index(j)
+            print(i, j, n)
+            strings[i].string[j] = data.alt[n]
+        except ValueError:
+            pass
+        j += CHUNK_SIZE
+    i += 1
+
+
+    return strings
+
+main_string = my_string()
+#main_string.print_chunk()
+print(''.join(main_string.string))
 
 data = my_data(pos, ref, alt)
 data.bubble_sort()
 
 print(data)
+    
+combinations = gen_comb(data, main_string)
+
+
+for i in range(main_string.cos):
+    #combinations[0].print_chunk()
+    print(i, ''.join(combinations[i].string))
